8245fd420bfb4f6729cb68d0ba519cea11afa7ee
Rosen <rosen.damyanov7@gmail.com>
2024-07-09 01:10:12 +0300

    Refactor code

--- Telerik_ForumTeamProject/Helpers/DateTimeFormatter.cs
+++ Telerik_ForumTeamProject/Helpers/DateTimeFormatter.cs
@@ -4,12 +4,6 @@ namespace Telerik_ForumTeamProject.Helpers
 {
     public class DateTimeFormatter
     {
-        private readonly DateTime _dateTime;
-        private readonly string _dateFormat;
-        public DateTimeFormatter(DateTime dateTime)
-        {
-            
-        }
         public static string FormatToStandard(DateTime dateTime)
         {
             string parsedDate = dateTime.ToString("dd.MM.yyyy HH:mm:s");
