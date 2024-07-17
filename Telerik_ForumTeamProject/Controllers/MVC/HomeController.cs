f94b37424a9bb6457171dac6c6fced8d683d0445
Rosen <rosen.damyanov7@gmail.com>
2024-07-17 20:13:26 +0300

    Added home controller

---
+++ Telerik_ForumTeamProject/Controllers/MVC/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Telerik_ForumTeamProject.Controllers.MVC
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
