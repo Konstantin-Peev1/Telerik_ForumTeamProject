880807a5dd379afd97d587a28a8bf86717e0fe9a
Konstantin <konstantin.i.peev@gmail.com>
2024-07-17 19:19:15 +0300

    Preparing Project for MVC Work

---
+++ Telerik_ForumTeamProjectMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Telerik_ForumTeamProjectMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
